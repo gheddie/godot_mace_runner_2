c1195485fe0c8c5e1c2e7df55e2a7e995ad2cb07
Stefan Schulz <stefan.schulz.1976@googlemail.com>
2025-11-15 14:59:02 +0100

    minor changes to buider sandbox

--- sandbox/builder/MaceBuilderSandbox.gd
+++ sandbox/builder/MaceBuilderSandbox.gd
@@ -3,6 +3,8 @@ extends Node3D
 @onready var addTrailButton: Button = $Buttons/AddTrail
 @onready var addForkButton: Button = $Buttons/AddFork
 
+@onready var camera: Camera3D = $Camera3D
+
 func _ready() -> void:
 	addTrailButton.pressed.connect(addTrail)
 	addForkButton.pressed.connect(addFork)
@@ -10,9 +12,17 @@ func _ready() -> void:
 func addTrail() -> void:
 	var element = MaceElementFactoryInstance.buildTrailElement()
 	add_child(element)
-	element.acceptPosition(MaceObjectManagerInstance.registerMaceElement(element))
+	var position = MaceObjectManagerInstance.registerMaceElement(element)
+	element.acceptPosition(position)
+	updateCameraPosition(position.x)
 	
 func addFork() -> void:
 	var element = MaceElementFactoryInstance.buildForkElement()
 	add_child(element)
-	element.acceptPosition(MaceObjectManagerInstance.registerMaceElement(element))
+	var position = MaceObjectManagerInstance.registerMaceElement(element)
+	element.acceptPosition(position)
+	updateCameraPosition(position.x)
+	
+func updateCameraPosition(xPosition: float) -> void:
+	camera.global_position.x = xPosition
+	print(str("updating camera position to --> ", str(camera.global_position)))
